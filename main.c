b9ee5072e95bb65404c757c100da4eceeea296f5
akshay734 <81257505+akshay734@users.noreply.github.com>
2024-12-08 22:11:38 +0530

    Update main.c
    simple change

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+typedef struct person{
+  char* name;
+  int* age;
+  char* designation;
+  int* height;
+  int* weight;
+};
+
+int main(int* argc ,char* argv[]){
+  person p;
+  p.name=argv[1];
+  p.age=argv[2];
+  p.designation=argv[3];
+  p.height=argv[4];
+  p.weight=argv[5];
+  
+}
